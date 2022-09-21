f0a967b37b6104db8a83d96fbe9f60c5d7fc0054
Tony <tony@mtrlabs.com>
2022-09-21 14:54:35 +0800

    Add handle_insurace_c_cover_m_buy_cover_v3 and handle_insurace_c_cover_m_cancel_cover handler functions

--- src/handle_provide_parameter.c
+++ src/handle_provide_parameter.c
@@ -1,7 +1,47 @@
 #include "insurace_plugin.h"
 
 // EDIT THIS: Remove this function and write your own handlers!
-static void handle_swap_exact_eth_for_tokens(ethPluginProvideParameter_t *msg, context_t *context) {
+// static void handle_swap_exact_eth_for_tokens(ethPluginProvideParameter_t *msg, context_t *context) {
+//     if (context->go_to_offset) {
+//         if (msg->parameterOffset != context->offset + SELECTOR_SIZE) {
+//             return;
+//         }
+//         context->go_to_offset = false;
+//     }
+//     switch (context->next_param) {
+//         case MIN_AMOUNT_RECEIVED:  // amountOutMin
+//             copy_parameter(context->amount_received,
+//                            msg->parameter,
+//                            sizeof(context->amount_received));
+//             context->next_param = PATH_OFFSET;
+//             break;
+//         case PATH_OFFSET:  // path
+//             context->offset = U2BE(msg->parameter, PARAMETER_LENGTH - 2);
+//             context->next_param = BENEFICIARY;
+//             break;
+//         case BENEFICIARY:  // to
+//             copy_address(context->beneficiary, msg->parameter, sizeof(context->beneficiary));
+//             context->next_param = PATH_LENGTH;
+//             context->go_to_offset = true;
+//             break;
+//         case PATH_LENGTH:
+//             context->offset = msg->parameterOffset - SELECTOR_SIZE + PARAMETER_LENGTH * 2;
+//             context->go_to_offset = true;
+//             context->next_param = TOKEN_RECEIVED;
+//             break;
+//         case TOKEN_RECEIVED:  // path[1] -> contract address of token received
+//             copy_address(context->token_received, msg->parameter, sizeof(context->token_received));
+//             context->next_param = UNEXPECTED_PARAMETER;
+//             break;
+//         // Keep this
+//         default:
+//             PRINTF("Param not supported: %d\n", context->next_param);
+//             msg->result = ETH_PLUGIN_RESULT_ERROR;
+//             break;
+//     }
+// }
+
+static void handle_insurace_c_cover_m_buy_cover_v3(ethPluginProvideParameter_t *msg, context_t *context) {
     if (context->go_to_offset) {
         if (msg->parameterOffset != context->offset + SELECTOR_SIZE) {
             return;
@@ -9,28 +49,26 @@ static void handle_swap_exact_eth_for_tokens(ethPluginProvideParameter_t *msg, c
         context->go_to_offset = false;
     }
     switch (context->next_param) {
-        case MIN_AMOUNT_RECEIVED:  // amountOutMin
-            copy_parameter(context->amount_received,
-                           msg->parameter,
-                           sizeof(context->amount_received));
-            context->next_param = PATH_OFFSET;
+        case INSURACE_C_COVER_M_BUY_COVER_V3_P_PRODUCTS_OFFSET:  // uint16[] products
+            context->next_param = UNEXPECTED_PARAMETER;
             break;
-        case PATH_OFFSET:  // path
-            context->offset = U2BE(msg->parameter, PARAMETER_LENGTH - 2);
-            context->next_param = BENEFICIARY;
+        // Keep this
+        default:
+            PRINTF("Param not supported: %d\n", context->next_param);
+            msg->result = ETH_PLUGIN_RESULT_ERROR;
             break;
-        case BENEFICIARY:  // to
-            copy_address(context->beneficiary, msg->parameter, sizeof(context->beneficiary));
-            context->next_param = PATH_LENGTH;
-            context->go_to_offset = true;
-            break;
-        case PATH_LENGTH:
-            context->offset = msg->parameterOffset - SELECTOR_SIZE + PARAMETER_LENGTH * 2;
-            context->go_to_offset = true;
-            context->next_param = TOKEN_RECEIVED;
-            break;
-        case TOKEN_RECEIVED:  // path[1] -> contract address of token received
-            copy_address(context->token_received, msg->parameter, sizeof(context->token_received));
+    }
+}
+
+static void handle_insurace_c_cover_m_cancel_cover(ethPluginProvideParameter_t *msg, context_t *context) {
+    if (context->go_to_offset) {
+        if (msg->parameterOffset != context->offset + SELECTOR_SIZE) {
+            return;
+        }
+        context->go_to_offset = false;
+    }
+    switch (context->next_param) {
+        case INSURACE_C_COVER_M_CANCEL_COVER_P_COVER_ID:  // uint256 coverId
             context->next_param = UNEXPECTED_PARAMETER;
             break;
         // Keep this
@@ -57,9 +95,10 @@ void handle_provide_parameter(void *parameters) {
     // EDIT THIS: adapt the cases and the names of the functions.
     switch (context->selectorIndex) {
         case INSURACE_C_COVER_M_BUY_COVER_V3:
-            handle_swap_exact_eth_for_tokens(msg, context);
+            handle_insurace_c_cover_m_buy_cover_v3(msg, context);
             break;
         case INSURACE_C_COVER_M_CANCEL_COVER:
+            handle_insurace_c_cover_m_cancel_cover(msg, context);
             break;
         default:
             PRINTF("Selector Index not supported: %d\n", context->selectorIndex);
